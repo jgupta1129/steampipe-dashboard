55c60ccd560b636502da0413ffce709e8584ae22
Jitender Gupta <guptajitender@johndeere.com>
2023-04-19 15:25:07 -0500

    removed

--- install_mods.sh
+++
@@ -1,3 +0,0 @@
-steampipe mod install github.com/turbot/steampipe-mod-aws-insights
-steampipe mod install github.com/turbot/steampipe-mod-aws-compliance
-steampipe mod install github.com/turbot/steampipe-mod-azure-insights
